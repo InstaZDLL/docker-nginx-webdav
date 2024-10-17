57ee9368b4d5a6cff59462c55e37f9f9ea83de00
InstaZDLL <github.105mh@8shield.net>
2024-10-17 08:02:46 +0200

    Adding .gitignore file and eeformatting the entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 : "${TLS_DISABLE:-true}"
 
-
 if [ "$TLS_DISABLE" = "false" ]; then
     sed -i 's/listen 80;/listen 443 ssl;/' /etc/nginx/conf.d/default.conf
 	sed -i '/listen 443 ssl;/a \
@@ -11,12 +10,13 @@ if [ "$TLS_DISABLE" = "false" ]; then
     ssl_protocols TLSv1.2 TLSv1.3;\
     ssl_ciphers HIGH:!aNULL:!MD5;' /etc/nginx/conf.d/default.conf
 	sed -i '1i server {\n    listen 80;\n    return 301 https://$host$request_uri;\n}' /etc/nginx/conf.d/default.conf
+	echo TLS configuration performed.
 fi
 
 if [[ -n "$USERNAME" ]] && [[ -n "$PASSWORD" ]]
 then
 	htpasswd -bc /etc/nginx/htpasswd $USERNAME $PASSWORD
-	echo Done.
+	echo Authentication configuration performed.
 else
     echo Using no auth.
 	sed -i 's%auth_basic "Restricted";% %g' /etc/nginx/conf.d/default.conf
