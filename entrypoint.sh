927dd6c70019a4bdb5f588cde0d45f9c48a8cd6b
InstaZDLL <github.105mh@8shield.net>
2024-10-17 07:45:11 +0200

    entrypoint tls configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,14 @@
 
 
 if [ "$TLS_DISABLE" = "false" ]; then
-    echo "[Info] The WORDPRESS_COMPOSE env var is set to True"
-    sleep 8
+    sed -i 's/listen 80;/listen 443 ssl;/' /etc/nginx/conf.d/default.conf
+	sed -i '/listen 443 ssl;/a \
+	\
+    ssl_certificate /ssl/certificate.pem;\
+    ssl_certificate_key /ssl/private.pem;\
+    ssl_protocols TLSv1.2 TLSv1.3;\
+    ssl_ciphers HIGH:!aNULL:!MD5;' /etc/nginx/conf.d/default.conf
+	sed -i '1i server {\n    listen 80;\n    return 301 https://$host$request_uri;\n}' /etc/nginx/conf.d/default.conf
 fi
 
 if [[ -n "$USERNAME" ]] && [[ -n "$PASSWORD" ]]
